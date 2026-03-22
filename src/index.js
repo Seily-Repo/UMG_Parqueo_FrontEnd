4e73f07a178516cf7524e26b8521becef058fd9a
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2026-03-21 18:45:10 -0600

    instalacion de librerias de diseño - 5

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@flaticon/flaticon-uicons/css/all/all.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
