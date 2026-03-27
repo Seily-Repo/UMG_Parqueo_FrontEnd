222da9a386abbfb66fa97cf628f678607ed83846
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2026-03-26 19:38:52 -0600

    Resolución de Bug por libreria react router

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,27 @@
 import { NavLink } from "react-router-dom";
-
 import "../styles/Barra.modules.css";
 import logo from "../images/logo.png";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { Offcanvas } from "bootstrap";
 
 const Nav = () => {
+  const offcanvasRef = useRef(null);
+  const offcanvasInstance = useRef(null);
+
   useEffect(() => {
-    const offcanvasElement = document.getElementById('offcanvasNavbar');
-    offcanvasElement?.addEventListener('hidden.bs.offcanvas', () => {
-      document.body.classList.remove('modal-open');
-    });
+    if (offcanvasRef.current) {
+      offcanvasInstance.current = new Offcanvas(offcanvasRef.current);
+    }
   }, []);
+
+  const openMenu = () => {
+    offcanvasInstance.current?.show();
+  };
+
+  const closeMenu = () => {
+    offcanvasInstance.current?.hide();
+  };
 
   return (
     <nav className="navbar navbar-expand-lg">
@@ -23,8 +33,7 @@ const Nav = () => {
         <button
           className="navbar-toggler d-lg-none"
           type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasNavbar"
+          onClick={openMenu}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -32,14 +41,14 @@ const Nav = () => {
         <div className="collapse navbar-collapse flex-row-reverse d-none d-lg-flex">
           <ul className="navbar-nav me-5">
             <li className="nav-item me-3">
-                <NavLink to="/" className="nav-link">
-                  <i className="fi fi-rr-home me-2"></i>Inicio
-                </NavLink>
+              <NavLink to="/" className="nav-link">
+                <i className="fi fi-rr-home me-2"></i>Inicio
+              </NavLink>
             </li>
             <li className="nav-item me-3">
-                <NavLink to="/perfil" className="nav-link">
-                  <i className="fi fi-rr-user me-2"></i>Perfil
-                </NavLink>
+              <NavLink to="/perfil" className="nav-link">
+                <i className="fi fi-rr-user me-2"></i>Perfil
+              </NavLink>
             </li>
             <li className="nav-item me-3">
               <a className="nav-link" href="#!">
@@ -55,30 +64,29 @@ const Nav = () => {
         </div>
 
         <div
-          className="offcanvas offcanvas-end d-lg-none navbarMovile"
-          tabIndex="-1"
-          id="offcanvasNavbar"
+          ref={offcanvasRef}
+          className="navbarMobile offcanvas offcanvas-end d-lg-none"
         >
           <div className="offcanvas-header">
             <h5 className="offcanvas-title navbar-brand">Menú</h5>
             <button
               type="button"
               className="btn-close"
-              data-bs-dismiss="offcanvas"
+              onClick={closeMenu}
             ></button>
           </div>
 
           <div className="offcanvas-body">
             <ul className="navbar-nav flex-column">
               <li className="nav-item mb-3">
-                  <NavLink to="/" className="nav-link">
-                    <i className="fi fi-rr-home me-2"></i>Inicio
-                  </NavLink>
+                <NavLink to="/" className="nav-link" onClick={closeMenu}>
+                  <i className="fi fi-rr-home me-2"></i>Inicio
+                </NavLink>
               </li>
               <li className="nav-item mb-3">
-                  <NavLink to="/perfil" className="nav-link">
-                    <i className="fi fi-rr-user me-2"></i>Perfil
-                  </NavLink>
+                <NavLink to="/perfil" className="nav-link" onClick={closeMenu}>
+                  <i className="fi fi-rr-user me-2"></i>Perfil
+                </NavLink>
               </li>
               <li className="nav-item mb-3">
                 <a className="nav-link" href="#!">
