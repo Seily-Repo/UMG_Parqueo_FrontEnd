c6889fd70b8c0e1983963219d828f5cd05ac9477
blopezv17 <blopezv17@gmail.com>
2026-03-27 21:20:41 -0600

    Función: Espacios para representación de parqueo

---
+++ src/components/Espacio.jsx
@@ -0,0 +1,18 @@
+export default function Espacio({ numero }) {
+  return (
+    <div
+      style={{
+        width: "60px",
+        height: "100px",
+        backgroundColor: "#ccc",
+        border: "1px solid black",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontWeight: "bold"
+      }}
+    >
+      {numero}
+    </div>
+  );
+}
