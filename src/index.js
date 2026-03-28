f6bb4e155264316a9ecfa4da47c10b1ce501d401
blopezv17 <blopezv17@gmail.com>
2026-03-27 21:26:01 -0600

    Función: Agregar ruta /parqueo al router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,26 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@flaticon/flaticon-uicons/css/all/all.css";
 import Perfil from "./perfil";
+import Parqueo from "./pages/Parqueo";
 
+export default function Espacio({ numero }) {
+  return (
+    <div
+      style={{
+        width: "60px",
+        height: "100px",
+        backgroundColor: "#ccc",
+        border: "1px solid black",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontWeight: "bold"
+      }}
+    >
+      {numero}
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -15,6 +34,7 @@ root.render(
       <Routes>
           <Route path="/" element={<App />} />
           <Route path="/perfil" element={<Perfil />} />
+          <Route path="/parqueo" element={<Parqueo />} />
         </Routes>
     </BrowserRouter>
   </React.StrictMode>,
