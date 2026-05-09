52f038c04a3545a0cf0cc6eac5fa05bc12449ea4
blopezv17 <blopezv17@gmail.com>
2026-05-08 22:26:00 -0600

    Cambio de estado de barra de resumen

--- src/pages/Parqueo.jsx
+++ src/pages/Parqueo.jsx
@@ -37,19 +37,28 @@ export default function Parqueo() {
       ES_Espacio: idEspacio,
       id_ciclo: 1,
       id_jornada: 1,
-      correlativo: "pi_3RDXTm2eZvKY1o2C1TjPKHmg"
+      correlativo: "TEST-123"
     };
 
     const res = await asignarEspacio(data);
 
     console.log("Asignación exitosa:", res.data);
 
+    // ACTUALIZAR ESPACIO LOCALMENTE
+    setEspacios(prev =>
+      prev.map(e =>
+        e.ES_Espacio === idEspacio
+          ? { ...e, ES_Estado: 0 }
+          : e
+      )
+    );
+
     alert("Espacio asignado correctamente");
 
   } catch (error) {
-    console.error("Error al asignar:", error.response.data);
+    console.error("Error al asignar:", error.response?.data);
   }
-};  
+};
 
 
 
