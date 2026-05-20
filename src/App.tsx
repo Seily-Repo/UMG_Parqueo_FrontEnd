882372167eac58cc8e08814b9cba4b294c9149c5
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2026-05-19 23:11:44 -0600

    basename agregado

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import ReporteAdministrativo from './pages/DashboardAdmin/reportes/administrativ
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/reportes">
       <Routes>
         <Route path="/" element={<SelectorRol />} />
         <Route path="/login" element={<Login />} />
