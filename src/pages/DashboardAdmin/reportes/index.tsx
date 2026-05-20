813b2143af2bbb54f635465f680bd38c6412eefe
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2026-05-19 22:57:36 -0600

    Update index.tsx

--- src/pages/DashboardAdmin/reportes/index.tsx
+++ src/pages/DashboardAdmin/reportes/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SideBarAdmin from "../../../components/SidebarAdmin";
-import "../../../styles/reportes.module.css";
+//import "@/styles/reportes.module.css";
 
 import {useNavigate} from "react-router-dom";
 
