95f82c0faff705fd94fd77c0e168f94f7019b60c
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2026-05-21 21:49:33 -0600

    Update index.tsx

--- src/pages/reportes/index.tsx
+++ src/pages/reportes/index.tsx
@@ -16,7 +16,7 @@ const Index = () => {
     if (adminGuardado && token) {
       setAdminLogueado(JSON.parse(adminGuardado));
     } else {
-      navigate('/login-admin');
+      navigate('http://10.0.40.10/login');
     }
     
   }, [navigate]);
