805ab1643d903403a6205a5d3cb7f016e2aaa704
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2026-05-21 22:13:01 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    strictPort: true,
+    hmr: {
+      host: "10.0.40.10",
+      protocol: "ws",
+      port: 3000,
+    }
   },
 })
