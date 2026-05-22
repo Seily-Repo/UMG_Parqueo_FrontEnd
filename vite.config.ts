c51bf341eb91a3c42e38b1a0ba976946dee93b39
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2026-05-21 22:38:45 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '10.0.40.10',
-    port: 3000,
-    strictPort: true,
-    hmr: {
-      host: "10.0.40.10",
-      protocol: "ws",
-      port: 3000,
-    }
-  },
 })
